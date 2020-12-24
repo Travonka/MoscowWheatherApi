c10a8a0ee9feda349aafdbe0a0f01c5fbb10c9fd
Travonka <travnikov2014@yandex.ru>
2020-12-24 18:18:33 +0300

    test

---
+++ Metar-decoder/Exception/ChainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metar_decoder.Exception
+{
+    public sealed class  ChainException : System.Exception
+    {
+    }
+    public static class Messages
+    {
+        public static string testErrorMessage = "lol kek cheburek";
+    }
+}
